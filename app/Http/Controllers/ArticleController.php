a84be2c85040e8a8d555f5624f56cfd22612bfd7
sohh <sohh.akasaka85@gmail.com>
2021-01-04 23:04:37 +0900

    initial commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+
+    public function index()
+    {
+        // Articleモデルの全データが(sortByDescメソッドで)最新の投稿日時順に並び替えられた上で$articles に代入
+        $articles = Article::all()->sortByDesc('created_at');
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+}
