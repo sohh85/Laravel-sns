abaab1bbfd20239f70f6eed399017408a39d87ae
sohh <sohh.akasaka85@gmail.com>
2021-01-04 23:56:55 +0900

    記事投稿ページにauthミドルウェアを使用し、ログイン済みじゃない場合ログイン画面に戻す

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@
 Auth::routes();
 // このWebアプリ（/）にアクセスされたら、 ArticleControllerコントローラーのindexメソッドを呼び出す
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index']);
+Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
