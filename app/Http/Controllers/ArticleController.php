e1a9248e72aaeb2f816bfc523fe9e0972809af3b
sohh <sohh.akasaka85@gmail.com>
2021-01-04 23:09:04 +0900

    記事登録画面表示のメソッドcreateをコントローラに記述

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,12 +8,16 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-
     public function index()
     {
         // Articleモデルの全データが(sortByDescメソッドで)最新の投稿日時順に並び替えられた上で$articles に代入
         $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');
     }
 }
