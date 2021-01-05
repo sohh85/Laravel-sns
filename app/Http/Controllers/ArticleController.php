a74203886075abea0d1630c4f86bc11dc77e2c84
sohh <sohh.akasaka85@gmail.com>
2021-01-05 10:39:55 +0900

    投稿された記事とタイトルをsaveメソッドで保存
    ・DIを使ってArticleRequestクラス、Articleクラスのインスタンスを自動生成
    ・$articleモデルのインスタンスに対し入力された内容と、userメソッドで投稿者のIDを取得して保存
    ・redirectメソッドで戻す

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -20,4 +20,17 @@ class ArticleController extends Controller
     {
         return view('articles.create');
     }
+
+    // 第一引数$requestはA(ArticleRequestクラスのインスタンス)と指定
+    // 指定することで、A以外のものが来たらTypeErrorという例外を発生させれる
+    // int とか string とかも指定できる 
+    // （DI）ララベルのコントローラーはメソッドの引数で型宣言を行うと、そのクラスのインスタンスが自動生成されメソッド内で使える
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
+    }
 }
