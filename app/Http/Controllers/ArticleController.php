beaad1d3064781f099ef53f09cd0fd61922a16da
sohh <sohh.akasaka85@gmail.com>
2021-01-05 11:26:39 +0900

    fillableプロパティを定義し、パラメータをtitleとnameのみにして不正な値を排除

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -24,12 +24,17 @@ class ArticleController extends Controller
     // 第一引数$requestはA(ArticleRequestクラスのインスタンス)と指定
     // 指定することで、A以外のものが来たらTypeErrorという例外を発生させれる
     // int とか string とかも指定できる 
-    // （DI）ララベルのコントローラーはメソッドの引数で型宣言を行うと、そのクラスのインスタンスが自動生成されメソッド内で使える
+    // （DI）コントローラーはメソッドの引数で型宣言を行うと、そのクラスのインスタンスが自動生成されメソッド内で使える
     public function store(ArticleRequest $request, Article $article)
     {
-        $article->title = $request->title;
-        $article->body = $request->body;
+        // Articleモデルのインスタンスである$articleのtitle、bodyプロパティに対し、
+        // 記事登録画面から送信されたPOSTリクエストのtitleと本文bodyの値をそれぞれ代入
+        // allメソッドで送信された値を配列で取得
+        $article->fill($request->all());
+        // リクエストのuserメソッドを使うとUserクラスのインスタンスにアクセスできる。
+        // そこからユーザーのidを取得し、user_idプロパティに代入
         $article->user_id = $request->user()->id;
+        // saveメソッドでarticlesテーブルにレコードを新規登録
         $article->save();
         return redirect()->route('articles.index');
     }
