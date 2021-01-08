22f2c0373378ee885aa6a8e71fae019b8b4c087e
sohh <sohh.akasaka85@gmail.com>
2021-01-09 00:00:14 +0900

    いいねテーブルのmigrationファイル作成

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 
 class Article extends Model
@@ -17,4 +18,9 @@ class Article extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function likes(): BelongsToMany //いいねの記事モデルとユーザーモデルの関係は「多対多」
+    { // belongsToMany(関係するモデル, 中間テーブル)
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    } //第二引数を省略すると、中間テーブル名は2つのモデル名の単数形をアルファベット順に結合した名前として処理
 }
