e1c1a9f67ab3f4d4925de8caad087ced08396845
sohh <sohh.akasaka85@gmail.com>
2021-01-09 00:22:11 +0900

    isLikedByメソッドつくり、記事モデルで呼び出されたときにいいね数計算し、そのユーザがいいねを押しているかチェック

--- app/Article.php
+++ app/Article.php
@@ -23,4 +23,11 @@ class Article extends Model
     { // belongsToMany(関係するモデル, 中間テーブル)
         return $this->belongsToMany('App\User', 'likes')->withTimestamps();
     } //第二引数を省略すると、中間テーブル名は2つのモデル名の単数形をアルファベット順に結合した名前として処理
+
+    public function isLikedBy(?User $user): bool // ?を付けると、その引数がnullであることも許容
+    {
+        return $user //三項演算子を使い$userがnullでなければ(bool)以下を返す
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false; // コレクションにはwhere(キー名, 値)がある
+    }
 }
