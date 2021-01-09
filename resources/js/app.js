2762c4bf61fe584594067d92aaa83c0f9d3eed72
sohh <sohh.akasaka85@gmail.com>
2021-01-09 22:33:13 +0900

    ArticleTagsInputタグをApp.jsで読み込み

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
+import ArticleTagsInput from './components/ArticleTagsInput'
 
 const app = new Vue({
     el: '#app',
