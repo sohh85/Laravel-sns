ae3c9c4fb332350cf5dbed346c4b3ca154b66981
sohh <sohh.akasaka85@gmail.com>
2021-01-14 10:23:15 +0900

    UserController.phpに@followersメソッドを定義。

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::prefix('articles')->name('articles.')->group(function () {
 });
 Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
 
-Route::prefix('users')->name('users.')->group(function () {
+Route::prefix('users')->name('users.')->group(function () { //groupのときだけネストできる
     Route::get('/{name}', 'UserController@show')->name('show');
     Route::get('/{name}/likes', 'UserController@likes')->name('likes');
     Route::get('/{name}/followings', 'UserController@followings')->name('followings');
