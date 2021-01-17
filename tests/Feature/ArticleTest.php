eb31261a5a9fe7dbfb3b74db797b1a6a34c5f794
sohh <sohh.akasaka85@gmail.com>
2021-01-17 17:20:32 +0900

    いいねしているユーザを渡したら正常にtrueが返るかのテストを追記

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Article;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,13 +12,28 @@ class ArticleTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testIsLikedByNull()
+    public function testIsLikedByNull() // isLikedByにnullを渡したら正常にfalseが返るか
     {
         // ファクトリによって生成されたArticleモデルがデータベースに保存され、さらに$articleに代入
         $article = factory(Article::class)->create();
 
+        // Articleクラスのインスタンスが代入された$articleがisLikedByメソッドを使用
         $result = $article->isLikedBy(null); // isLikedByメソッドを使用
 
+        // assertFalseメソッドで引数がfalseかどうかをテスト
         $this->assertFalse($result);
+    }
+
+    public function testIsLikedByTheUser() // いいねシているユーザを渡したら正常にtrueが返るか
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        // Userモデルのlikesメソッドを呼び出し、
+        $article->likes()->attach($user);
+
+        // Articleクラスのインスタンスが代入された$articleで、isLikedByメソッドを使用
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
     }
 }
