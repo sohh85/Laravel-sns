d97a7afb52e7abed131fde2a10c7148273fa59fc
sohh <sohh.akasaka85@gmail.com>
2021-01-17 12:15:13 +0900

    assertRedirectメソッドで未ログインユーザが正しくリダイレクトされているかテストをArticleControllerTest.phpに記入

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,11 +11,21 @@ class ArticleControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    // 記事一覧表示で正しいステータスが返っているかチェック
     public function testIndex()
     {
         $response = $this->get(route('articles.index'));
 
         $response->assertStatus(200)
             ->assertViewIs('articles.index');
+    }
+
+    // 未ログイン状態のユーザに対するテスト
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('articles.create'));
+
+        // assertRedirectメソッドでは引数に渡したURLにリダイレクトされたかテスト
+        $response->assertRedirect(route('login'));
     }
 }
